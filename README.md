d056316ab51ae09d4d937ab247a2b56501375896
Primož Hadalin <hadalin.primoz@gmail.com>
2014-11-11 23:00:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Chrome extension that removes fedora youtube commenters.
+
+Profile IDs are stored in ```hidefedora/resources/fedoras.json```
---
+++ hidefedora/src/content.js
@@ -0,0 +1,36 @@
+var resource = null;
+
+var endsWith = function(str, suffix) {
+	return str.indexOf(suffix, str.length - suffix.length) !== -1;
+};
+
+var removeFedora = function(outerSelector, innerSelector) {
+	$(outerSelector).each(function(index, element) {
+		var href = $(element).find(innerSelector).attr("href");
+
+		if(typeof _.find(resource.fedoras, function(fedora) { 
+			return endsWith(href, fedora);
+		}) !== "undefined") {
+			$(this).remove();
+		}
+	});
+};
+
+$.getJSON("https://raw.githubusercontent.com/hadalin/chrome-hidefedora/master/hidefedora/resources/fedoras.json", function(res) {
+	resource = res;
+});
+
+// $.getJSON(chrome.extension.getURL("/resources/fedoras.json"), function(res) {
+// 	resource = res;
+// });
+
+$(function() {
+
+	setInterval(function() {
+		if(resource !== null) {
+			removeFedora(".Yp.yt.Xa", ".ve.oba.HPa > a");
+			removeFedora(".Ik.Wv", ".fR > a");
+		}
+	}, 2000);
+
+});
