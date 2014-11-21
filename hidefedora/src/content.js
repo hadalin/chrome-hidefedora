de904907c222fb816bba4ceb4c0357ae5440ab66
Primoz Hadalin <primoz.hadalin@xlab.si>
2014-11-21 16:40:13 +0100

    Optimize node removal

--- hidefedora/src/content.js
+++ hidefedora/src/content.js
@@ -16,6 +16,13 @@ var removeFedora = function(outerSelector, innerSelector) {
 	});
 };
 
+var execute = function() {
+	if(resource !== null) {
+		removeFedora(".Yp.yt.Xa", ".ve.oba.HPa > a");
+		removeFedora(".Ik.Wv", ".fR > a");
+	}
+};
+
 $.getJSON("https://raw.githubusercontent.com/hadalin/chrome-hidefedora/master/hidefedora/resources/fedoras.json", function(res) {
 	resource = res;
 });
@@ -26,11 +33,27 @@ $.getJSON("https://raw.githubusercontent.com/hadalin/chrome-hidefedora/master/hi
 
 $(function() {
 
-	setInterval(function() {
-		if(resource !== null) {
-			removeFedora(".Yp.yt.Xa", ".ve.oba.HPa > a");
-			removeFedora(".Ik.Wv", ".fR > a");
-		}
-	}, 2000);
+	// Set MutationObserver
+	var target = document.querySelector('.yJa');
+	 
+	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+	var observer = new MutationObserver(function() {
+		execute();
+	});
+	 
+	var config = { childList: true, subtree: true };
+	 
+	observer.observe(target, config);
+
+	// Execute removal a couple of times before MutationObserver kicks in
+	var counter = 0;
+	var interval = setInterval(function() {
+		execute();
+
+    	counter++;
+    	if(counter === 8) {
+        	clearInterval(interval);
+    	}
+	}, 1000);
 
 });
